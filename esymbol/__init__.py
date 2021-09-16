c13d1c1d7fda18b604115e9cb40fa6711bb1b441
youryanmi <64118037+Tikmoing@users.noreply.github.com>
2021-09-16 16:05:11 +0800

    Add files via upload

---
+++ esymbol/__init__.py
@@ -0,0 +1,4 @@
+"symbol operator for all"
+__all__ = "esymbol"
+__version__ = "1.0.0"
+
---
+++ esymbol/punctuation.py
@@ -0,0 +1,11 @@
+from esymbol import hanzi
+import string
+
+#string format
+en = string.punctuation
+cn = hanzi.punctuation
+
+#list type
+en_list = list(en)
+cn_list = list(cn)
+
